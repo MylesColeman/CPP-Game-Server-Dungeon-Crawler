a8e80dc65522f3f68a5e9fc15afe4e2e7dfa311c
Myles Coleman <mylesjcoleman1011@gmail.com>
2026-04-29 10:38:14 +0100

    fix(mechanic): increase the delay between world state messages
    This whilst technically making the game slightly less synced, offers a smoother experience.

--- GameServer.h
+++ GameServer.h
@@ -62,7 +62,7 @@ public:
     static constexpr float TICK_RATE_MS = 16.0f;
     static constexpr int BROADCAST_INTERVAL = 6;
     static constexpr size_t MAX_HISTORY_TICKS = 30;
-    static constexpr float NETWORK_RATE_LIMIT = 0.05f; // One message every 50ms
+    static constexpr float NETWORK_RATE_LIMIT = 1.f; // One message every second
 
     // Connection Limits
     static constexpr size_t MAX_PLAYERS = 2;
